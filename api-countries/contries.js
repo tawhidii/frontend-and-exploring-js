60beb4336417151d05b0da97fb0450d84b008e15
barii <khademul.py@gmail.com>
2021-07-13 14:29:16 +0600

    Added coutry details view

--- api-countries/contries.js
+++ api-countries/contries.js
@@ -29,5 +29,23 @@ function displayData(data){
 
 
 function countryDetails(countryName){
-    console.log(countryName)
+    fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
+    .then(response=>response.json())
+    .then(data=>renderDetails(data[0]))
+}
+
+
+const renderDetails = country => { 
+    console.log(country)
+    const mainDiv = document.getElementById('country-list')
+    mainDiv.style.display = "none"
+    const countryDetailDiv = document.getElementById('country-details')
+    countryDetailDiv.innerHTML = `
+        <h1>Country Name : ${country.name} </h1>
+        <h2>Population : ${country.population} </h2>
+        <h2> Area : ${country.area}</h2>
+        <img src="${country.flag}">
+    `
+
+
 }
