996b094e1e02c0b1818e5cd7a6437a239454a37c
barii <khademul.py@gmail.com>
2021-07-18 01:46:28 +0600

    added play-with-typescript

---
+++ play-with-typescript/dist/test.js
@@ -0,0 +1,16 @@
+"use strict";
+// function addTwoNumber(numOne:number,numTwo:number){
+//     let result:number = numOne+numTwo
+//     return result
+// }
+let messi = {
+    name: 'messi',
+    age: 34,
+    salary: 300000000,
+};
+let ronaldo = {
+    name: 'Ronaldo',
+    age: 34,
+    salary: 300000000,
+};
+console.log(ronaldo.salary);
---
+++ play-with-typescript/test.ts
@@ -0,0 +1,35 @@
+// function addTwoNumber(numOne:number,numTwo:number){
+//     let result:number = numOne+numTwo
+//     return result
+// }
+
+// let addNum: (x:number,y:number) => number
+
+// addNum = (x,y) => x+y
+// addNum(2,4)
+
+
+// Interface 
+
+interface Player {
+    name:string,
+    age:number,
+    gender?:string, // value not mandatory
+    salary:number
+}
+
+let messi: Player = {
+    name:'messi',
+    age:34,
+    salary:300000000,
+
+
+}
+
+let ronaldo: Player = {
+    name:'Ronaldo',
+    age:34,
+    salary:300000000,
+}
+
+console.log(ronaldo.salary)
