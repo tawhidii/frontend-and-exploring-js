d79d9f3df35001723909dcfecac81676a8088f6c
pr0m3th30u$ <barii.py@gmail.com>
2021-10-07 23:05:50 +0600

    guess game 94% done

--- Guess Num/script.js
+++ Guess Num/script.js
@@ -1,10 +1,16 @@
 'use strict';
 
 const  secretNumber= Math.trunc(Math.random()* 20) + 1
-// document.querySelector('.number').textContent = secretNumber
+console.log(secretNumber)
+let score = Number(document.querySelector('.score').textContent)
+let highScore = Number(document.querySelector('.highscore').textContent)
+console.log(highScore)
 
 
-const checkingNumber = (secretNumber) => {
+
+
+
+const checkingNumber = (secretNumberm,score,highScore) => {
     const checkButton = document.querySelector('.check')
     checkButton.addEventListener('click',()=>{
         const message = document.querySelector('.message')
@@ -16,14 +22,34 @@ const checkingNumber = (secretNumber) => {
             guessNumber>secretNumber ? 
                 message.textContent = '📈 Guess is high !!':
                 message.textContent = '📉 Guess is low !!' 
+                score--
+                document.querySelector('.score').textContent = score
         }
         if(guessNumber === secretNumber){
-            message.textContent = '🎉 correct' 
+            message.textContent = '🎉 Hurrah you win the game !!' 
             document.querySelector('body').style.backgroundColor = 'green'
+            document.querySelector('.number').textContent = secretNumber
+            if(score>highScore){
+                highScore = score
+                document.querySelector('.highscore').textContent = highScore
+            }
+            
         }
-        
+        if(score < 1 ){
+            message.textContent = 'You lost the game 😿 !!'
+            checkButton.disabled = true
+        }
 
     })
 }
 
-checkingNumber(secretNumber)
+checkingNumber(secretNumber,score,highScore)
+
+const resetGame = () => {
+    const againBtn = document.querySelector('.again')
+    againBtn.addEventListener('click',()=>{
+        location.reload()
+    })
+
+}
+resetGame()
