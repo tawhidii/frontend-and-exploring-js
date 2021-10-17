82159d41769228f171c53aaddf85af6b2411c30c
barii <barii.py@gmail.com>
2021-10-17 18:30:13 +0600

    Added pop up example

--- pop-up/script.js
+++ pop-up/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const buttonCloseModal = document.querySelector('.close-modal')
+const buttonOpenModal = document.querySelectorAll('.show-modal')
+
+const openModal = function(){
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+const closeModal = function(){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+for(let i = 0;i<buttonOpenModal.length;i++){
+    buttonOpenModal[i].addEventListener('click',openModal)
+}
+
+
+overlay.addEventListener('click',closeModal)
+
+buttonCloseModal.addEventListener('click',closeModal)
+
