705e28a06180ad0e78df9c08a9cba6a1fa036e47
Khademul Bari <barii.py@gmail.com>
2021-11-18 20:27:40 +0600

    Added solve

---
+++ challenges/ch-1.js
@@ -0,0 +1,71 @@
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+        'Neuer',
+        'Pavard',
+        'Martinez',
+        'Alaba',
+        'Davies',
+        'Kimmich',
+        'Goretzka',
+        'Coman',
+        'Muller',
+        'Gnarby',
+        'Lewandowski',
+        ],
+        [
+        'Burki',
+        'Schulz',
+        'Hummels',
+        'Akanji',
+        'Hakimi',
+        'Weigl',
+        'Witsel',
+        'Hazard',
+        'Brandt',
+        'Sancho',
+        'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+    'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+    printGoals:function(...players){
+        players.forEach(player=>console.log(player))
+    }
+};
+
+// Player One 
+const [players1,players2] = game.players
+
+// Gk 
+const [gk,...fieldPlayers] = players1
+// All players 
+const allPlayers = [...players1,...players2]
+// Players One final 
+const players1Final = [...players1,'Thiago','Coutinho','Perisic']
+
+// game odds 
+const {team1,x:draw,team2} = game.odds
+
+
+game.printGoals('Ronaldo','Beckham','Salah','Messi','Neymar')
+
+
+
+// console.log(team1,draw,team2) 
+// console.log(players1Final)
+
+
+// console.log(allPlayers)
+// console.log(gk,fieldPlayers)
+
+
